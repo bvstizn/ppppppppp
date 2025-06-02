71863a41edb1ae6cbc5611040e29275080707bd5
bvstizn <144767208+bvstizn@users.noreply.github.com>
2025-06-02 04:29:56 -0400

    Update Cuenta.java

--- Cuenta.java
+++ Cuenta.java
@@ -1,13 +1,33 @@
-
 package com.mycompany.bostonbanks2;
+
+import java.util.Random;
 
 public abstract class Cuenta {
     protected int numero;
     protected int saldo;
 
+    // Constructor con número aleatorio
+    public Cuenta() {
+        this.numero = generarNumeroCuentaAleatorio();
+        this.saldo = 0;
+    }
+
+    // Constructor con número explícito
     public Cuenta(int numero) {
         this.numero = numero;
         this.saldo = 0;
+    }
+
+    // Constructor con número explícito y saldo inicial
+    public Cuenta(int numero, int saldoInicial) {
+        this.numero = numero;
+        this.saldo = saldoInicial;
+    }
+
+    // Generador de número aleatorio de 9 dígitos
+    public static int generarNumeroCuentaAleatorio() {
+        Random rand = new Random();
+        return 100_000_000 + rand.nextInt(900_000_000);
     }
 
     public int getNumero() {
